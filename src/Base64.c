f27b8a8ecd18a71d3b0bc8d0e296aeb246d8c5dc
Kelpering <tristan8272@gmail.com>
2024-01-19 10:52:52 -0700

    dwasd

--- src/Base64.c
+++ src/Base64.c
@@ -9,19 +9,19 @@ ByteArr B64toByte(char* B64String)
 
 char* BytetoB64(uint8_t* Array, size_t Size)
 {
-    size_t NewSize = Size + (Size % 3);
+    // size_t NewSize = Size + (Size % 3);
     // Split every 3 bytes into 4 chars
     // Pad array first?
-    uint8_t* NewArr = malloc(NewSize);
-    for (size_t i = 0; i < Size; i++)
-    {
-        NewArr[i] = Array[i];
-    }
-    for (size_t i = Size; i < NewSize; i++)
-    {
-        NewArr[i] = 0;
-        // Should run once, twice, or never
-    }
+    // uint8_t* NewArr = malloc(NewSize);
+    // for (size_t i = 0; i < Size; i++)
+    // {
+    //     NewArr[i] = Array[i];
+    // }
+    // for (size_t i = Size; i < NewSize; i++)
+    // {
+    //     NewArr[i] = 0;
+    //     // Should run once, twice, or never
+    // }
 
     // NewSize should be divisible by 3 bytes
     // For every 3 bytes, there should be 4 chars
@@ -34,15 +34,31 @@ char* BytetoB64(uint8_t* Array, size_t Size)
         // ((NewArr[i+1] & 0b1111) << 2) | (NewArr[i+2] >> 6); // Third letter
         // (NewArr[i+2] & 0b111111);                           // Final Letter
 
-    char* B64String = malloc((NewSize/3) * 4)
-    for (size_t i = 0, j = 0; i < Size; i+=3)
+    char* B64String = malloc(((Size/3) * 4) + 1);
+    B64String[((Size/3) * 4) + 1] = '\0';
+    for (size_t i = 0, j = 0; i < Size - (Size % 3); i+=3)
     {
-        B64String[j++] = Base64Arr[(NewArr[i] >> 2)];
-        B64String[j++] = Base64Arr[((NewArr[i] & 0b11) << 4) | (NewArr[i+1] >> 4)];
-        B64String[j++] = Base64Arr[((NewArr[i+1] & 0b1111) << 2) | (NewArr[i+2] >> 6)];
-        B64String[j++] = Base64Arr[(NewArr[i+2] & 0b111111)];
-        // I am unsure if this is correct, as the pad has to be '='. This is never reached
-        //! Either the Base64Arr[] is wrong, or the pad byte is special and must be applied later
-        //! Size - 3 might be the new for loop, different step for padding.
+        B64String[j++] = Base64Arr[(Array[i] >> 2)];
+        B64String[j++] = Base64Arr[((Array[i] & 0b11) << 4) | (Array[i+1] >> 4)];
+        B64String[j++] = Base64Arr[((Array[i+1] & 0b1111) << 2) | (Array[i+2] >> 6)];
+        B64String[j++] = Base64Arr[(Array[i+2] & 0b111111)];
     }
+    if ((Size % 3) == 1)
+    {
+        // No idea if this is right
+        B64String[((Size/3) * 4) - 3] = Base64Arr[(Array[Size-2] >> 2)];
+        B64String[((Size/3) * 4) - 2] = Base64Arr[((Array[Size-2] & 0b11) << 4) | (Array[Size-1] >> 4)];
+        B64String[((Size/3) * 4) - 1] = Base64Arr[((Array[Size-1] & 0b1111) << 2) | (Array[Size] >> 6)];
+        B64String[((Size/3) * 4) - 0] = Base64Arr[(Array[Size] & 0b111111)];
+    }
+    else if ((Size % 3) == 2)
+    {
+
+    }
+    
+    // There are either 0, 1, or 2 bytes to add
+    // 0: done
+    // 1: 8 '0' bits are needed
+    // 2: 16 '0' bits are needed
+
 }
