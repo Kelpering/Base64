03b317bca8640e26af1ec8fe2a27bf8e9ec70b84
Kelpering <tristan8272@gmail.com>
2024-01-26 20:36:01 -0700

    Finalized Implementation

--- include/Base64.h
+++ include/Base64.h
@@ -14,14 +14,14 @@ typedef struct
 /// @returns A boolean True/False.
 bool ValidateB64(char* B64String);
 
-/// @brief Translates a Base64 string into a byte array. (RFC4748 Compliant) -DOUBLE CHECK IF TRUE-
+/// @brief Translates a Base64 string into a byte array.
 /// @param B64String A Base64 string to be translated, null terminated.
 /// @returns A ByteArr struct, containing an allocated uint8_t array pointer and the Size of said array.
 /// @warning Returns an allocated array. MUST be de-allocated to prevent memory leak.
 /// @note If B64String is found to be an invalid Base64 string, the function returns (ByteArr) {NULL, 0}.
 ByteArr B64toByte(char* B64String);
 
-/// @brief Translate a byte array into a Base64 string. (RFC4648 Compliant) -DOUBLE CHECK IF TRUE-
+/// @brief Translate a byte array into a Base64 string.
 /// @param Array A byte array to be translated to Base64.
 /// @param Size The size of the byte array to translate.
 /// @returns An allocated Base64, null terminated string.
